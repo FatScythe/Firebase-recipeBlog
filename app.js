68ed0482dd7c5270859d946e985bdb6058ad7cc9
abdullahi fahm <holadhayo28@gmail.com>
2022-07-03 12:53:50 +0100

    Solving get issue

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const panels = document.querySelectorAll('.panel');
 const addBtn = document.querySelector('.add');
 const form = document.querySelector('form');
 
+// Getting Collections
+
 
 chevrons.forEach(chevron => {
     chevron.addEventListener('click', () => {
@@ -33,70 +35,79 @@ addBtn.addEventListener('click', function () {
 // Form validation
 
 validateTitle = () => {
-    let regex = /^[a-zA-Z0-9]{4,20}$/
-    if(form.title.value.match(regex)) {
-        console.log('yes');
+    const error = document.querySelector('.error-title');
+    let regex =/^.{1,40}$/;
+    let title = form.title.value;
+    
+    if(title.match(regex)) {
+        form.title.style.outline = '1px solid green';
+        error.textContent = '';
         return true;
-    } else {
-        console.log('no');
+    } 
+
+    if(!title.match(regex)) {
+        form.title.style.outline = '1px solid red';
+        error.textContent = 'Title is required';
         return false;
     }
 }
+   
 
 validateAuthor = () => {
     let regex = /^(\w+ )+\w+$/;
+    const error = document.querySelector('.error-author');
+    let author = form.author.value;
 
-	if(form.author.value.length == 0) {
-		// form.author.textContent += '<p>fullname is required</p>';
-        console.log('length is zero');
+	if(author.length == 0) {
+		error.innerHTML = '<p>This field cannot be empty</p>';
 		return false;
 	}
 
-	if(!form.author.value.match(regex)){
-		// form.author.textContent += '<p>Write full name</p>';
-        console.log('doesnot match');
+	if(!author.match(regex)){
+		error.innerHTML = '<p>fullname is required</p>';
 		return false;
-        
 	}
 
+    error.innerHTML = '';
     form.author.style.border = '1px solid green';
-    console.log('matches');
-	return true;	
+	return true;
 }
+	
 
-// console.log(form.content.outerHTML);
 
-validateContent = () => {
-    let regex = /^.{5,300}$/;
+validateContent = () => { 
+    let regex = /^.{1,1000}$/;
+    const error = document.querySelector('.error-content');
+    let content = form.content.value;
 
-    if(form.content.value.length == 0) {
-		// form.content.textContent += '<p>fullname is required</p>';
-        console.log('length is zero');
+    if(content.length == 0) {
+		error.textContent = 'content cannot be empty';
 		return false;
 	}
     
-	if(!form.content.value.match(regex)){
-		// form.content.textContent += '<p>Write full name</p>';
-        console.log('doesnot match');
+	if(!content.match(regex)){
+		error.textContent = 'Add more content';
 		return false;
-
 	}
 
+    error.textContent = '';
     form.content.style.border = '1px solid green';
-    console.log('matches');
 	return true;
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // validateTitle();
-    // validateAuthor();
-    // validateContent();
 
-    // if(!validateTitle() || !validateAuthor() || !validateContent()) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const error = document.querySelector('.error-form');
+    
+        if(!validateTitle() || !validateAuthor() || !validateContent()) {
+                error.textContent = 'Form is not valid';
+        } else {
+            error.textContent = '';
+        }
+    
+    });
 
-    // }
-})
 
 
 
